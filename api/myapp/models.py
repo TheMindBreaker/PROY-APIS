bf1c0dc0ba2344987feb413ea4a84158c3107ff1
TheMindBreaker <A01229632@tec.mx>
2023-11-30 19:04:18 -0600

    add

---
+++ api/myapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass  # Add additional fields here if needed
+
+# Create your models here.
